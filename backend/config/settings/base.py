ddd21830729b806bcaadd18f6ba6559fc0fd3b3d
APPLE <apple@MacBook-Pro.local>
2026-08-13 23:37:37 +0530

    Configure PostgreSQL database for production

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -18,6 +18,24 @@ ALLOWED_HOSTS = os.environ.get(
     "localhost,127.0.0.1,backend",
 ).split(",")
 # Application definition
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    import dj_database_url
+
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 INSTALLED_APPS = [
     # Django Apps
